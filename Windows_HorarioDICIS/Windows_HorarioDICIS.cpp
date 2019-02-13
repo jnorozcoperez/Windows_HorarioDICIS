b3a2980860182f7998957f6a58041e47211777ed
jnorozcoperez <jn.orozcoperez@ugto.mx>
2019-02-12 19:25:39 -0600

    Add project files.

---
+++ Windows_HorarioDICIS/Windows_HorarioDICIS.cpp
@@ -0,0 +1,12 @@
+﻿#include "stdafx.h"  //________________________________________ Windows_HorarioDICIS.cpp
+#include "Windows_HorarioDICIS.h"
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE , LPTSTR cmdLine, int cmdShow){
+	Windows_HorarioDICIS app;
+	return app.BeginDialog(IDI_Windows_HorarioDICIS, hInstance);
+}
+
+void Windows_HorarioDICIS::Window_Open(Win::Event& e)
+{
+}
+
---
+++ Windows_HorarioDICIS/Windows_HorarioDICIS.h
@@ -0,0 +1,27 @@
+﻿#pragma once  //______________________________________ Windows_HorarioDICIS.h  
+#include "Resource.h"
+class Windows_HorarioDICIS: public Win::Dialog
+{
+public:
+	Windows_HorarioDICIS()
+	{
+	}
+	~Windows_HorarioDICIS()
+	{
+	}
+protected:
+	//______ Wintempla GUI manager section begin: DO NOT EDIT AFTER THIS LINE
+	void InitializeGui()
+	{
+		this->Text = L"Windows_HorarioDICIS";
+	}
+	void Window_Open(Win::Event& e);
+	void GetDialogTemplate(DLGTEMPLATE& dlgTemplate)
+	{
+		dlgTemplate.style = DS_CENTER | DS_MODALFRAME | WS_POPUP | WS_VISIBLE | WS_CAPTION | WS_SYSMENU;
+	}
+	bool EventHandler(Win::Event& e)
+	{
+		return false;
+	}
+};
